987bd0005f3384ca8fe9f126b6efeb411da5ccba
maguowei <imaguowei@gmail.com>
2022-06-26 13:08:51 +0800

    refactor

---
+++ today.py
@@ -0,0 +1,64 @@
+import json
+import datetime
+from pathlib import Path
+from abc import ABC, abstractmethod
+
+
+class Today(ABC):
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+        self.date = datetime.date.today()
+        self.latest_data = self.get_latest_data()
+
+    def get_latest_data(self):
+        filename = self.get_filename_by_ext('json')
+        try:
+            with open(filename) as f:
+                data = json.load(f)
+                return data
+        except FileNotFoundError:
+            return []
+
+    def get_filename_by_ext(self, ext):
+        filename = f'data/{self.name}/{ext}/{self.date}.{ext}'
+        return filename
+
+    @abstractmethod
+    def crawler(self) -> list[dict]:
+        pass
+
+    def export(self):
+        data = self.crawler()
+        self.merge_data(data)
+        self.dump_json()
+        self.dump_md()
+
+    def merge_data(self, data):
+        titles = [item['title'] for item in self.latest_data]
+        new_count = 0
+        for item in data:
+            if item['title'] not in titles:
+                self.latest_data.append(item)
+                new_count += 1
+
+        print(f'merge data, count: {len(self.latest_data)}, new: {new_count}')
+        return self.latest_data
+
+    def dump_json(self):
+        filename = self.get_filename_by_ext('json')
+        file = Path(filename)
+        file.parent.mkdir(exist_ok=True, parents=True)
+        with file.open('w+') as f:
+            json.dump(self.latest_data, f, ensure_ascii=False, indent=2)
+
+    def dump_md(self):
+        filename = self.get_filename_by_ext('md')
+        file = Path(filename)
+        file.parent.mkdir(exist_ok=True, parents=True)
+        with file.open('w+') as f:
+            f.writelines(f'# {self.desc}\n\n')
+            f.writelines(f'{self.date}\n\n')
+            f.writelines(f'--- \n')
+            mds = [f"{i + 1}. [{hot['title']}]({hot['url']})\n" for i, hot in enumerate(self.latest_data)]
+            f.writelines(mds)
--- weibo.py
+++ weibo.py
@@ -1,66 +1,23 @@
-import datetime
-import json
 import requests
+from urllib.parse import urlencode
+from today import Today
 
 
-URL = 'https://m.weibo.cn/api/container/getIndex?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C&extparam=filter_type%3Drealtimehot%26mi_cid%3D100103%26pos%3D0_0%26c_type%3D30%26display_time%3D1540538388&luicode=10000011&lfid=231583'
+class WeiboHot(Today):
+    name = 'weibo_hot'
+    desc = '微博热搜'
+    url = 'https://m.weibo.cn/api/container/getIndex?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C&extparam=filter_type%3Drealtimehot'
 
-
-class Weibo:
-    @classmethod
-    def get_weibo_hots(cls):
-        r = requests.get(URL)
+    def crawler(self):
+        r = requests.get(self.url)
         hots = r.json()['data']['cards'][0]['card_group']
-        data = [{'title': hot['desc'], 'url': hot['scheme']} for hot in hots]
+
+        data = []
+        for item in hots:
+            query = urlencode({'q': f"#{item['desc']}#", 'Refer': 'top'})
+            data.append({'title': item['desc'], 'url': f"https://s.weibo.com/weibo?{query}"})
         return data
-
-    @classmethod
-    def get_latest_hot_data(cls, filename):
-        try:
-            with open(filename) as f:
-                data = json.load(f)
-                return data
-        except FileNotFoundError:
-            return []
-
-    @classmethod
-    def export_hots(cls):
-        today = datetime.date.today()
-        data = cls.get_weibo_hots()
-
-        raw_filename = f'data/raw/{today}.json'
-        md_filename = f'data/md/{today}.md'
-
-        old_data = cls.get_latest_hot_data(raw_filename)
-
-        data = cls.merge_hots(old_data, data)
-
-        cls.dump_hots_json(raw_filename, data)
-        cls.dump_hots_md(md_filename, data)
-
-    @classmethod
-    def merge_hots(cls, old_data, new_data):
-        titles = [hot['title'] for hot in old_data]
-        new_count = 0
-        for hot in new_data:
-            if hot['title'] not in titles:
-                old_data.append(hot)
-                new_count += 1
-
-        print(f'热搜总计: {len(old_data)}, 新增: {new_count}')
-        return old_data
-
-    @classmethod
-    def dump_hots_json(cls, filename, data):
-        with open(filename, 'w+') as f:
-            json.dump(data, f, ensure_ascii=False, indent=2)
-
-    @classmethod
-    def dump_hots_md(cls, filename, data):
-        with open(filename, 'w+') as f:
-            mds = [f"{i+1}. [{hot['title']}]({hot['url']})\n" for i, hot in enumerate(data)]
-            f.writelines(mds)
 
 
 if __name__ == '__main__':
-    Weibo.export_hots()
+    WeiboHot(WeiboHot.name, WeiboHot.desc).export()
