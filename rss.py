a8f5d08f0cb59c2e70ea1d812f840d2b733597f0
maguowei <imaguowei@gmail.com>
2022-06-27 11:41:36 +0800

    add rss support

--- rss.py
+++ rss.py
@@ -1,43 +1,38 @@
-import json
+from datetime import datetime, timezone, timedelta
+from time import mktime
 import yaml
 import feedparser
+import requests
+
+from today import Today, DEFAULT_HEADERS
 
 
-def get_config() -> dict:
-    data = None
+class RSS(Today):
+    def __init__(self, name, desc, icon, url):
+        self.name = name
+        self.desc = desc
+        self.icon = icon
+        self.url = url
+        super().__init__()
 
-    with open('feedme.yaml') as f:
+    def crawler(self):
+        r = requests.get(self.url, headers=DEFAULT_HEADERS)
+        news = feedparser.parse(r.content).entries
+
+        data = []
+        for new in news:
+            published_parsed = new.published_parsed
+            utc_time = datetime.fromtimestamp(mktime(published_parsed)).replace(tzinfo=timezone.utc)
+            bj_time = utc_time.astimezone(timezone(timedelta(hours=8)))
+            publish_time = bj_time.strftime('%Y-%m-%d %H:%M:%S')
+            if bj_time.date() == datetime.today().date():
+                item = {'title': new.title, 'url': new.link, 'summary': new.summary, 'publish_time': publish_time}
+                data.append(item)
+        return data
+
+
+if __name__ == '__main__':
+    with open('rss.yml') as f:
         data = yaml.safe_load(f)
-
-    return data
-
-
-config = get_config()
-
-title = config['title']
-sources = config['sources']
-
-data = []
-
-for source in sources:
-    d = feedparser.parse(source['href'])
-
-    items = []
-    for entrie in d.entries:
-        items.append(
-            {
-                'title': entrie.title,
-                'link': entrie.link,
-                # 'published': entrie.published,
-            }
-        )
-
-    source_data = {
-        'title': d.feed.title,
-        'link': d.feed.link,
-        'items': items,
-    }
-
-    data.append(source_data)
-
-print(json.dumps(data, ensure_ascii=False))
+        for source in data['sources']:
+            RSS(**source).export()
