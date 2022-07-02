3fb96f95a69e11d033249bb24be4810509f579ea
maguowei <imaguowei@gmail.com>
2022-07-02 22:50:15 +0800

    update sql_import add archive date

--- sql_import.py
+++ sql_import.py
@@ -1,6 +1,7 @@
 import os
 import json
 import sqlite3
+from utils.logger import logger
 
 
 JSON_DATA_PATH = 'data/feeds/json'
@@ -10,18 +11,18 @@ def sql_import():
     conn = sqlite3.connect('today.db')
     c = conn.cursor()
 
-    table_create_sql = 'CREATE TABLE IF NOT EXISTS feed (id INTEGER PRIMARY KEY AUTOINCREMENT, data json)'
+    table_create_sql = 'CREATE TABLE IF NOT EXISTS feed (id INTEGER PRIMARY KEY AUTOINCREMENT, data json, archive_date date)'
     c.execute(table_create_sql)
 
-    sql = 'INSERT INTO feed (data) VALUES (?)'
+    sql = 'INSERT INTO feed (data, archive_date) VALUES (?, ?)'
 
-    for file in os.listdir(JSON_DATA_PATH):
+    for file in sorted(os.listdir(JSON_DATA_PATH)):
         filepath = f'{JSON_DATA_PATH}/{file}'
         with open(filepath) as f:
-            print(f'process file: {filepath}')
+            logger.info(f'process file: {filepath}')
             data = json.load(f)
             for feed in data:
-                c.execute(sql, (json.dumps(feed, ensure_ascii=False),))
+                c.execute(sql, (json.dumps(feed, ensure_ascii=False), file.rstrip('.json')))
 
             conn.commit()
 
